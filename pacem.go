ed524c6b87cfdf66da5a9f908b4603b47c5dc726
vimproved <vimproved@inventati.org>
2022-11-03 14:04:26 -0400

    i cannot believe this works

--- pacem.go
+++ pacem.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"math/rand"
 	"net/http"
 	"os"
@@ -12,15 +13,19 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 type Score struct {
-	KeriLee  string
+	KeriLee string
 	Caroline string
-	Horn     string
-	Sax      string
-	Bass     string
+	Horn string
+	Sax string
+	Bass string
 }
+
+var inputs = make(map[string]int)
+var generateCompleted = make(chan any)
 
 var templates = template.Must(
 	template.ParseFiles(
@@ -32,6 +37,8 @@ var templates = template.Must(
 		"bassTemplate.ly",
 	),
 )
+
+var sections = [2]string{"intro", "test"}
 
 func partReader(title string, part string) string {
 	entries := 0
@@ -48,20 +55,19 @@ func partReader(title string, part string) string {
 
 func partConcatenator(part string) string {
 	var concatenatedPart strings.Builder
-	sectiondirs, _ := os.ReadDir("sections")
-	for _, sectiondir := range sectiondirs {
-		concatenatedPart.WriteString(partReader(sectiondir.Name(), part))
+	for _, sectiondir := range sections {
+		concatenatedPart.WriteString(partReader(sectiondir, part))
 	}
 	return concatenatedPart.String()
 }
 
 func scoreGenerator() {
-	p := Score{
-		KeriLee:  partConcatenator("KeriLee"),
+	p := Score {
+		KeriLee: partConcatenator("KeriLee"),
 		Caroline: partConcatenator("Caroline"),
-		Horn:     partConcatenator("Horn"),
-		Sax:      partConcatenator("Sax"),
-		Bass:     partConcatenator("Bass"),
+		Horn: partConcatenator("Horn"),
+		Sax: partConcatenator("Sax"),
+		Bass: partConcatenator("Bass"),
 	}
 	parts := [6]string{"score", "kerilee", "caroline", "horn", "sax", "bass"}
 	for _, part := range parts {
@@ -89,24 +95,68 @@ func inputHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Submit Number</h1>"+
 	"<form action=\"/submit/\" method=\"POST\">"+
 	"<textarea name=\"body\"></textarea><br>"+
+	"<input type=\"radio\" id=\"kerilee\" name=\"performer\" value=\"Keri Lee\">"+
+	"<label for=\"kerilee\">Keri Lee</label><br>"+
+	"<input type=\"radio\" id=\"caroline\" name=\"performer\" value=\"Carolyn\">"+
+	"<label for=\"caroline\">Caroline</label><br>"+
+	"<input type=\"radio\" id=\"horn\" name=\"performer\" value=\"Horn\">"+
+	"<label for=\"horn\">Horn</label><br>"+
+	"<input type=\"radio\" id=\"sax\" name=\"performer\" value=\"Sax\">"+
+	"<label for=\"sax\">Sax</label><br>"+
+	"<input type=\"radio\" id=\"bass\" name=\"performer\" value=\"Bass\">"+
+	"<label for=\"bass\">Bass</label><br>"+
 	"<input type=\"submit\" value=\"Save\">"+
 	"</form>")
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
 	choice, err := strconv.Atoi(r.FormValue("body"))
+	performer := r.FormValue("performer")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	rand.Seed(int64(choice))
-	scoreGenerator()
-	io.WriteString(w, "number submitted and score randomized")
+	inputs[performer] = choice
+	go waitForGenerate(w, r, performer)
+	fmt.Print(inputs)
+	http.Redirect(w, r, "/wait/" + strings.ToLower(strings.ReplaceAll(performer, " ", "")), http.StatusFound)
+}
+
+func waitHandler(w http.ResponseWriter, r *http.Request) {
+	performer := r.URL.Path[len("/wait/"):]
+	if len(inputs) < 5 {
+		fmt.Fprintf(w, "<head><meta http-equiv=\"refresh\" content=\"5\" /></head><body>Number submitted, waiting for other performers</body>")
+		return
+	}
+	http.Redirect(w, r, "/" + performer, http.StatusFound)
+}
+
+func waitForGenerate(w http.ResponseWriter, r *http.Request, performer string) {
+	generateCompleted <- struct{}{}
+}
+
+func waitToGenerate() {
+	for true {
+		for len(inputs) < 5 {
+			time.Sleep(1 * time.Second)
+		}
+		var seed int64 = 0
+		for _, v := range inputs {
+			v = int(math.Pow(float64(v), math.Mod(float64(v), 11)))
+			seed += int64(v)
+		}
+		rand.Seed(seed)
+		scoreGenerator()
+		inputs = make(map[string]int)
+		<- generateCompleted
+	}
 }
 
 func main() {
 	http.HandleFunc("/", partHandler)
 	http.HandleFunc("/submit/", submitHandler)
 	http.HandleFunc("/input/", inputHandler)
+	http.HandleFunc("/wait/", waitHandler)
+	go waitToGenerate()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
